fb9fd894bb86d70785483b71f9c7977f46a2fc2f
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-04-25 11:45:34 +0800

    Functionality + flow working for iOS with new icon set

--- lib/widgets/sign_in_button.dart
+++ lib/widgets/sign_in_button.dart
@@ -13,7 +13,7 @@ class SignInButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height * 0.07;
-    final double width = MediaQuery.of(context).size.width * 0.55;
+    final double width = MediaQuery.of(context).size.width * 0.6;
 
     return Align(
       alignment: AlignmentDirectional(0, yAlignment),
